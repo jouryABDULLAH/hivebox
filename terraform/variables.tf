5c67e0a7a56c9db8d84f865e1d4f6ca8a177f41d
Joori <jours-222@outlook.com>
2026-05-09 07:12:20 -0700

    chore(terraform): update output and variables

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type = string
+  default = "hivebox-495409"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-c"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "hivebox-gke"
+}
